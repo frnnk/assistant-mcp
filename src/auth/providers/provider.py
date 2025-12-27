34ca95e00651d934e17fcd186d546872471c61b0
Frank <ffliu926@gmail.com>
2025-12-27 00:20:08 -0500

    added parent oauth interfaces

---
+++ src/auth/providers/provider.py
@@ -0,0 +1,49 @@
+"""
+Provides an interface for common-ground OAuth client responsibilities for downstream MCP integrations
+and clients.
+"""
+
+from abc import ABC, abstractmethod
+from typing import Optional, Sequence
+from auth.tokens.token import OAuthToken
+
+class OAuthProvider(ABC):
+    """
+    Provides a parent interface for OAuth 2.0 providers.
+
+    Handles the common OAuth responsibilities:
+    - Get token given a principal id, if it exists
+    - If token is expired, refresh if possible
+    - Generate end-user auth url
+    - Replace with new token from callback
+    """
+    def __init__(self):
+        self.token: Optional[OAuthToken] = None
+        self.scopes: Optional[Sequence[str]] = None
+    
+    def _set_access_token(self, token: OAuthToken) -> None:
+        self.token = token
+    
+    # @abstractmethod
+    # def _refresh_access_token(self) -> bool:
+    #     return self.token.refresh()
+
+    @abstractmethod
+    def _get_access_token(self, principal_id):
+        pass
+
+    @abstractmethod
+    def _generate_auth_url(self):
+        pass
+
+    @abstractmethod
+    def get_access_token(self, principal_id: str, scopes: Sequence[str]):
+        pass
+
+        
+
+
+
+
+
+
---
+++ src/auth/tokens/token.py
@@ -0,0 +1,22 @@
+"""
+Provides an interface for tokens, corresponding to OAuth client responsibilities. Does not dictate 
+specific token fields, but provides needed methods.
+"""
+
+from abc import ABC, abstractmethod
+
+class OAuthToken(ABC):
+    """
+    Provides a parent interface for OAuth 2.0 tokens.
+
+    Handles the common OAuth responsibilities for tokens:
+    - Check token for expiration
+    - Refresh token if possible
+    """
+    @abstractmethod
+    def check_not_expired(self):
+        pass
+
+    @abstractmethod
+    def refresh(self):
+        pass
